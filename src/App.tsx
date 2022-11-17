9925d435531bf6d1d81f4d0556f6507165271f16
Marc Bernstein <marcbernstein98@gmail.com>
2022-11-17 01:12:42 -0500

    add nav menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { blueGrey, grey } from '@mui/material/colors';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 import ScrollingBackground from './components/ScrollingBackground';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: blueGrey,
+    divider: blueGrey[700],
+    background: {
+      default: blueGrey[900],
+      paper: blueGrey[900],
+    },
+    text: {
+      primary: '#fff',
+      secondary: grey[500],
+    },
+  }
+})
 
 function App() {
 
   return (
-    <div>
-      <ScrollingBackground />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <ResponsiveAppBar />
+      {/* <ScrollingBackground /> */}
+    </ThemeProvider>
   );
 }
 
