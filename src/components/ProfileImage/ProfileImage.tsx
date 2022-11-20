e0b2b39494f63600e27f24a398b0bbb185fd2dd0
Marc Bernstein <marcbernstein98@gmail.com>
2022-11-19 21:42:26 -0500

    add photo, text and animation

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -3,18 +3,22 @@ import "./ProfileImage.css";
 
 export default function ProfileImage(): JSX.Element {
   return (
-    // <Grid container justifyContent="right" sx={{
-    //   marginTop: "8%",
-    //   marginLeft: "-15%"
-    // }}>
     <Grid container spacing={2}>
       <Grid item xs={6} sx={{
-        marginTop: "8%"
+        marginTop: "3%"
       }}>
-        <h1>Placeholder text</h1>
+        <Box
+          component="h1"
+          display="flex"
+          justifyContent="center"
+          sx={{
+            fontSize: "xx-large"
+          }}
+        >Placeholder text</Box>
+
       </Grid>
       <Grid item xs={6} sx={{
-        marginTop: "8%"
+        marginTop: "3%"
       }}>
       <Box
         component="img"
