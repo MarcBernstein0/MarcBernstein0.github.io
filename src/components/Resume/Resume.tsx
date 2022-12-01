17c217a470bee82faf804167a0d26f717c9a12cb
Marc Bernstein <ma.bernstein@northeastern.edu>
2022-12-01 16:23:24 -0500

    resume is now visible

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,6 +1,14 @@
+import { Box } from "@mui/material";
+
 function Resume() {
   return (
-    <iframe src={"./Bernstein_Marc_Resume_8_5_2022.pdf"} />
+    <Box
+      component="iframe"
+      src={require("./Bernstein_Marc_Resume_8_5_2022.pdf")}
+    >
+
+    </Box>
+    // <iframe src={require("./Bernstein_Marc_Resume_8_5_2022.pdf")} width="1000px" height="100%" />
   );
 }
 // /Users/marcbernstein/Documents/projects/personal/MarcBernstein0.github.io/src/assets/Bernstein_Marc_Resume_8_5_2022.pdf
