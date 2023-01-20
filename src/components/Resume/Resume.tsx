c5511dd92f08c422e84f2dad351cc20f9d5b333c
Marc Bernstein <marcbernstein98@gmail.com>
2023-01-19 21:36:48 -0500

    add new resume

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -14,12 +14,13 @@ function Resume() {
         display="flex"
         alignItems="center"
         justifyContent="center"
-        src={require("../../assets/Bernstein_Marc_Resume_8_5_2022.pdf")}
+        src={require("../../assets/Marc_Bernstein_Software_Engineering_Resume_1_19_2022.pdf")}
         className="Fade-In"
         />
     </Grid>
 
   );
 }
+// /home/marc/Projects/MarcBernstein0.github.io/src/assets/Marc_Bernstein_Software_Engineering_Resume_1_19_2022.pdf
 // /Users/marcbernstein/Documents/projects/personal/MarcBernstein0.github.io/src/assets/Bernstein_Marc_Resume_8_5_2022.pdf
 export default Resume;
